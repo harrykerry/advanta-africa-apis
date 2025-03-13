923bec321229f974f04c135963c59ec292b69780
harrykerry <haroldkerry@gmail.com>
2025-03-14 00:05:38 +0300

    Initial Commit

--- docs/.vuepress/.temp/pages/sms-api/bulk-sms.html.js
+++ docs/.vuepress/.temp/pages/sms-api/bulk-sms.html.js
@@ -1,5 +1,5 @@
 import comp from "/home/harold/Documents/Projects/advanta-api-docs/docs/.vuepress/.temp/pages/sms-api/bulk-sms.html.vue"
-const data = JSON.parse("{\"path\":\"/sms-api/bulk-sms.html\",\"title\":\"SEND BULK SMS\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"SEND BULK SMS\"},\"headers\":[{\"level\":2,\"title\":\"POST Method\",\"slug\":\"post-method\",\"link\":\"#post-method\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"sms-api/bulk-sms.md\"}")
+const data = JSON.parse("{\"path\":\"/sms-api/bulk-sms.html\",\"title\":\"SEND BULK SMS\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"SEND BULK SMS\"},\"headers\":[{\"level\":2,\"title\":\"POST Method\",\"slug\":\"post-method\",\"link\":\"#post-method\",\"children\":[]}],\"git\":{\"updatedTime\":1741899483000,\"contributors\":[{\"name\":\"harrykerry\",\"username\":\"harrykerry\",\"email\":\"haroldkerry@gmail.com\",\"commits\":1,\"url\":\"https://github.com/harrykerry\"}],\"changelog\":[{\"hash\":\"f03da0d7b216cec07746048fad2d865c24c74957\",\"time\":1741899483000,\"email\":\"haroldkerry@gmail.com\",\"author\":\"harrykerry\",\"message\":\"Initial commit\"}]},\"filePathRelative\":\"sms-api/bulk-sms.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
